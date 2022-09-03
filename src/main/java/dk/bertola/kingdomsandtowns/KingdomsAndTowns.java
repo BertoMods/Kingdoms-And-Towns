93a5fef3e0756073e49aa8baade76d87f0d67eb1
BertoLaDK <lucasmulberg@live.dk>
2022-09-03 15:57:25 +0200

    Cleaned up and made it runnable.

---
+++ src/main/java/dk/bertola/kingdomsandtowns/KingdomsAndTowns.java
@@ -0,0 +1,19 @@
+package dk.bertola.kingdomsandtowns;
+
+import net.minecraftforge.fml.common.Mod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(KingdomsAndTowns.MOD_ID)
+public class KingdomsAndTowns {
+
+    public static final String MOD_ID = "kingdomsandtowns";
+    public static final String MOD_NAME = "Kingdoms And Towns";
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public KingdomsAndTowns(){
+
+    }
+}
